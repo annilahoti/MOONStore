f09f24ec40f86e28c00987f5137b2880f42e5f3d
Endrit <em67512@ubt-uni.net>
2023-11-24 11:04:45 +0100

    Products border in man section and favorite function

---
+++ funksionet.js
@@ -0,0 +1,13 @@
+
+
+function fav1(){
+    var icon1 = document.getElementById('fav2');
+    
+
+    if(icon1.style.color == "black"){
+        icon1.style.color = 'red';
+    }else{
+        icon1.style.color = "black";
+    }
+}
+
